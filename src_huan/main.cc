62e7fff8ef8666da1052324f16c9e83b0747e966
Huan Xu <hxu296@wisc.edu>
2023-08-27 15:15:20 -0400

    docs: add comment to some confusing parts

--- src_huan/main.cc
+++ src_huan/main.cc
@@ -6,7 +6,8 @@
 
 color ray_color(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
+    auto a = 0.5*(unit_direction.y() + 1.0); // y in [-1, 1], a in [0, 1]
+    // blend value = (1-a)*start_value + a*end_value
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
@@ -20,8 +21,8 @@ int main() {
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Camera
-    auto focal_length = 1.0;
+    // Camera, centered at (0,0,0), looking at the negative z-axis
+    auto focal_length = 1.0; // distance from the camera center to viewport
     auto viewport_height = 2.0;
     // use the actual aspect ratio of the image to calculate the viewport width
     auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
@@ -32,12 +33,16 @@ int main() {
     auto viewport_v = vec3(0, -viewport_height, 0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    // pixel width and height in the viewport coordinate (u, v)
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
+    // viewport_upper_left is at (-viewport_width/2, +viewport_height/2, -focal_length)
+    // this is also the location of the upper left corner of the pixel (0,0)
     auto viewport_upper_left = camera_center
                              - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    // pixel00_loc is the center of the pixel (0,0), in the viewport coordinate (u, v)
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
@@ -46,6 +51,10 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
+            // for each pixel, we create a ray from the camera center to the pixel center
+            // and then calculate the color of the pixel based on the ray
+            
+            // pixel center is the center of the pixel (i,j), in the viewport coordinate (u, v)
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
